d43cc2e8c48a532ba9732bce450f2ba1664ec08b
Benjamin <ayzenbergb@gmail.com>
2017-02-20 16:05:12 -0800

    replace images functional

--- app.js
+++ app.js
@@ -7,21 +7,21 @@ var images = [
   new Image('img/bathroom.jpg', 'bathroom'),
   new Image('img/boots.jpg', 'boots'),
   new Image('img/breakfast.jpg', 'breakfast'),
-  new Image('img/bubblegum', 'bubblegum'),
-  // new Image('img/chair.jpg', 'chair'),
-  // new Image('img/cthulhu.jpg', 'cthulhu'),
-  // new Image('img/dog-duck.jpg', 'dog-duck'),
-  // new Image('img/dragon.jpg', 'dragon'),
-  // new Image('img/pen.jpg', 'pen'),
-  // new Image('img/pet-sweep.jpg', 'pet-sweep'),
-  // new Image('img/scissors.jpg', 'scissors'),
-  // new Image('img/sharks.jpg', 'sharks'),
-  // new Image('img/sweep.jpg', 'sweep'),
-  // new Image('img/tauntaun.jpg', 'tauntaun'),
-  // new Image('img/unicorn.jpg', 'unicorn'),
-  // new Image('img/usb.gif', 'usb'),
-  // new Image('img/water-can.jpg', 'water-can'),
-  // new Image('img/wine-glass.jpg', 'wine-glass')
+  new Image('img/bubblegum.jpg', 'bubblegum'),
+  new Image('img/chair.jpg', 'chair'),
+  new Image('img/cthulhu.jpg', 'cthulhu'),
+  new Image('img/dog-duck.jpg', 'dog-duck'),
+  new Image('img/dragon.jpg', 'dragon'),
+  new Image('img/pen.jpg', 'pen'),
+  new Image('img/pet-sweep.jpg', 'pet-sweep'),
+  new Image('img/scissors.jpg', 'scissors'),
+  new Image('img/shark.jpg', 'sharks'),
+  new Image('img/sweep.jpg', 'sweep'),
+  new Image('img/tauntaun.jpg', 'tauntaun'),
+  new Image('img/unicorn.jpg', 'unicorn'),
+  new Image('img/usb.gif', 'usb'),
+  new Image('img/water-can.jpg', 'water-can'),
+  new Image('img/wine-glass.jpg', 'wine-glass')
 ];
 
 console.log('---------List of Images---------');
@@ -33,6 +33,9 @@ console.log(lastSet);
 var currentSet = [];
 console.log('----current Set----');
 console.log(currentSet);
+
+// Form Parent Node
+var formEl = document.getElementById('option-set');
 
 // simple image node creator
 function createImage(url, alt, parentNode) {
@@ -57,34 +60,21 @@ Image.prototype.votePercentage = function () {
   this.shownPercentage = this.votes / this.timesShown * 100;
 };
 
-// function image(url, alt) {
-//   var newImage = new MakeImage(url, alt);
-//   images.push(newImage);
-// }
-
 // Display
-var formEl = document.getElementById('option-set');
 
 // Index Generator
 function randomIndexGenerator() {
   return Math.floor(Math.random() * images.length);
 }
 
-// function lastSetCheck() {
-// }
-
-// currentSetCheck
-// function currentSetCheck() {
-// }
-
-function showImage() {
+//display a set of images
+function setImage() {
   lastSet = currentSet;
   currentSet = [];
   for (var i = 0; i < 3; i++) {
     var index = randomIndexGenerator();
     var currentImage = images[index];
     currentSet.push(currentImage);
-    // lastSetCheck();
     //lastSet check
     for (var iLast = 0; iLast < lastSet.length; iLast++) {
       if (lastSet[iLast] === currentImage){
@@ -102,16 +92,32 @@ function showImage() {
       }
     }
   }
+  console.log('current set');
+  console.log(currentSet);
+  console.log('last set');
+  console.log(lastSet);
 }
 
-showImage();
-console.log('----run 1 current set----');
-console.log(currentSet);
-showImage();
-console.log('----run 2 current set');
-console.log(currentSet);
-console.log('----run 2 last set');
-console.log(lastSet);
+function showImage() {
+  for (var j = 0; j < currentSet.length; j++) {
+    createImage(currentSet[j].url, currentSet.alt, formEl);
+  }
+}
+
+function burnTheChildren() {
+  while (formEl.hasChildNodes()) {
+    formEl.removeChild(formEl.firstChild);
+  }
+}
+
+function print() {
+  burnTheChildren();
+  setImage();
+  showImage();
+}
+
+print();
+
 // vote tracker
 
 // results
